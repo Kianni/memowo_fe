e8d733bf3060783d291fa1178efcdb58a3b88665
Kirill Nikolaev <nikolaeff.k.a@gmail.com>
2024-12-20 09:29:53 +0200

    add tests to frontend

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,32 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from './App';
+import fetchMock from 'jest-fetch-mock';
 
-test('renders learn react link', () => {
+fetchMock.enableMocks();
+
+beforeEach(() => {
+  fetch.resetMocks();
+});
+
+test('fetches words and updates state on button click', async () => {
+  const mockWords = {
+    data: { words: [{ spanish: 'hola', english: 'hello' }] },
+  };
+  fetch.mockResponseOnce(JSON.stringify(mockWords));
+
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const button = screen.getByText('Learn Sea Creatures');
+  fireEvent.click(button);
+
+  expect(fetch).toHaveBeenCalledTimes(1);
+  expect(fetch).toHaveBeenCalledWith(expect.any(String), expect.any(Object));
+});
+
+test('renders learn sea creatures button', () => {
+  render(<App />);
+  const buttonElement = screen.getByText(/learn sea creatures/i);
+  expect(buttonElement).toBeInTheDocument();
 });
