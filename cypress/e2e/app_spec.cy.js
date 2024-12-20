2fc2803844e9b7dc018225738f7ecd1b57ef5b9e
Kirill Nikolaev <nikolaeff.k.a@gmail.com>
2024-12-20 16:45:12 +0200

    MW-50 add proper front-end url to integration test

--- cypress/e2e/app_spec.cy.js
+++ cypress/e2e/app_spec.cy.js
@@ -3,7 +3,7 @@
 describe('App Integration Test', () => {
   it('fetches words from backend and updates state on button click', () => {
     // Visit the app
-    cy.visit('http://localhost:3000'); // Adjust the URL based on your local setup
+    cy.visit(`${process.env.REACT_APP_FRONTEND_URL}`); // Adjust the URL based on your local setup
 
     // Simulate button click to fetch words
     cy.get('button').contains('Learn Sea Creatures').click();
