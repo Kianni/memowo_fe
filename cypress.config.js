a732b740f032ad7ec97b403bec933f28571b4547
Kirill Nikolaev <nikolaeff.k.a@gmail.com>
2024-12-20 16:21:54 +0200

    MW-50 try to test e2e newly deployed code

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'memowo-fe-bmeed5dkbpcre6e6.canadacentral-01.azurewebsites.net',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
