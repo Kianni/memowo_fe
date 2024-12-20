86c7fc0038fa0b92b9f7f74b138897a13a4f5a76
Kirill Nikolaev <nikolaeff.k.a@gmail.com>
2024-12-20 16:26:34 +0200

    MW-50 add http://

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'memowo-fe-bmeed5dkbpcre6e6.canadacentral-01.azurewebsites.net',
+    baseUrl: 'https://memowo-fe-bmeed5dkbpcre6e6.canadacentral-01.azurewebsites.net',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
